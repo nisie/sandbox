add5aaea741ce1c59c2a4006e228fb9b7b188b89
nisie <nathania.sutedja@tokopedia.com>
2019-01-31 10:55:44 +0700

    tag

--- app/src/main/java/com/nisie/sandbox/MainActivity.java
+++ app/src/main/java/com/nisie/sandbox/MainActivity.java
@@ -44,78 +44,78 @@ public class MainActivity extends AppCompatActivity {
         info = findViewById(R.id.info);
         imageContainer = findViewById(R.id.image_container);
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int screenHeight = displayMetrics.heightPixels;
-        screenWidth = displayMetrics.widthPixels;
-        infoImg.setText("Width : " + screenWidth + " Height : " + screenHeight);
-
-        ConstraintLayout.LayoutParams parms = new ConstraintLayout.LayoutParams(screenWidth, screenWidth);
-        imageContainer.setLayoutParams(parms);
-        imageContainer.requestLayout();
-
-        testImage.setOnTouchListener(new View.OnTouchListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        info.setText("Touch coordinates DOWN: " +
-                                String.valueOf(Math.round(event.getX()) + " x " + String.valueOf
-                                        (Math.round(event.getY()))));
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        v.performClick();
-                        info.setText("Touch coordinates UP: " +
-                                String.valueOf(Math.round(event.getX()) + " x " + String.valueOf
-                                        (Math.round(event.getY()))));
-
-                        tag = new View(MainActivity.this);
-                        tagText = new TextView(MainActivity.this);
-                        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
-                                ARROW_WIDTH,
-                                ARROW_HEIGHT
-                        );
-
-                        int xTagPos = getXPosition(event.getX());
-                        int yTagPos = getYPosition(event.getY());
-                        params.leftMargin = xTagPos;
-                        params.topMargin = yTagPos;
-                        setArrow(yTagPos, tag);
-                        tag.setOnTouchListener(onTagTouch());
-
-                        ConstraintLayout.LayoutParams textParams = new ConstraintLayout.LayoutParams(
-                                TEXT_WIDTH,
-                                TEXT_HEIGHT
-                        );
-                        textParams.leftMargin = getXTextPosition(xTagPos);
-                        textParams.topMargin = getYTextPosition(yTagPos);
-                        tagText.setBackgroundColor(getResources().getColor(R.color.black));
-                        tagText.setTextColor(getResources().getColor(R.color.white));
-                        tagText.setText("Test Tag");
-
-                        tagText.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                int indexDelete = v.getId();
-                                int indexDeleteTag = indexDelete - 1;
-                                imageContainer.removeView(findViewById(indexDelete));
-                                imageContainer.removeView(findViewById(indexDeleteTag));
-                            }
-                        });
-                        tag.setId(index);
-                        tagText.setId(index + 1);
-                        imageContainer.addView(tag, params);
-                        imageContainer.addView(tagText, textParams);
-                        index += 2;
-
-                        break;
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//        int screenHeight = displayMetrics.heightPixels;
+//        screenWidth = displayMetrics.widthPixels;
+//        infoImg.setText("Width : " + screenWidth + " Height : " + screenHeight);
+//
+//        ConstraintLayout.LayoutParams parms = new ConstraintLayout.LayoutParams(screenWidth, screenWidth);
+//        imageContainer.setLayoutParams(parms);
+//        imageContainer.requestLayout();
+//
+//        testImage.setOnTouchListener(new View.OnTouchListener() {
+//            @SuppressLint("SetTextI18n")
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        info.setText("Touch coordinates DOWN: " +
+//                                String.valueOf(Math.round(event.getX()) + " x " + String.valueOf
+//                                        (Math.round(event.getY()))));
+//                        break;
+//                    case MotionEvent.ACTION_UP:
+//                        v.performClick();
+//                        info.setText("Touch coordinates UP: " +
+//                                String.valueOf(Math.round(event.getX()) + " x " + String.valueOf
+//                                        (Math.round(event.getY()))));
+//
+//                        tag = new View(MainActivity.this);
+//                        tagText = new TextView(MainActivity.this);
+//                        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+//                                ARROW_WIDTH,
+//                                ARROW_HEIGHT
+//                        );
+//
+//                        int xTagPos = getXPosition(event.getX());
+//                        int yTagPos = getYPosition(event.getY());
+//                        params.leftMargin = xTagPos;
+//                        params.topMargin = yTagPos;
+//                        setArrow(yTagPos, tag);
+//                        tag.setOnTouchListener(onTagTouch());
+//
+//                        ConstraintLayout.LayoutParams textParams = new ConstraintLayout.LayoutParams(
+//                                TEXT_WIDTH,
+//                                TEXT_HEIGHT
+//                        );
+//                        textParams.leftMargin = getXTextPosition(xTagPos);
+//                        textParams.topMargin = getYTextPosition(yTagPos);
+//                        tagText.setBackgroundColor(getResources().getColor(R.color.black));
+//                        tagText.setTextColor(getResources().getColor(R.color.white));
+//                        tagText.setText("Test Tag");
+//
+//                        tagText.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                int indexDelete = v.getId();
+//                                int indexDeleteTag = indexDelete - 1;
+//                                imageContainer.removeView(findViewById(indexDelete));
+//                                imageContainer.removeView(findViewById(indexDeleteTag));
+//                            }
+//                        });
+//                        tag.setId(index);
+//                        tagText.setId(index + 1);
+//                        imageContainer.addView(tag, params);
+//                        imageContainer.addView(tagText, textParams);
+//                        index += 2;
+//
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
 
 
     }
@@ -187,15 +187,19 @@ public class MainActivity extends AppCompatActivity {
     private int getYPosition(float y) {
         if (y > screenWidth - ARROW_HEIGHT)
             return Math.round(screenWidth - ARROW_HEIGHT);
-        else return Math.round(y);
+        else if (y > screenWidth / 2)
+            return Math.round(y - ARROW_HEIGHT);
+        else
+            return Math.round(y);
+
     }
 
     private int getXPosition(float x) {
         if (x > screenWidth - ARROW_WIDTH)
             return Math.round(screenWidth - ARROW_WIDTH);
-        else if (x > screenWidth / 2)
-            return Math.round(x - ARROW_WIDTH);
-        else
+        else if (x < ARROW_WIDTH) {
             return Math.round(x);
+        } else
+            return Math.round(x - (ARROW_WIDTH / 2));
     }
 }
